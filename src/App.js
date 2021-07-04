9c13fa6ce577b8b50120a820767789d6dfeaf78c
karthick <karthylock@gmail.com>
2021-07-04 17:14:18 +0530

    complete UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import "./App.css";
 import Navigation from "./component/Navigation/Navigation";
-import Logo from './component/Logo/Logo';
+import Logo from "./component/Logo/Logo";
+import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
+import Rank from "./component/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+       density: {
+         enable: true,
+         value_area: 800
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
