db3fd232032c35c31877a08d36a2e959dcd61cbd
karthick <karthylock@gmail.com>
2021-07-04 16:15:20 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import Navigation from './component/Navigation/Navigation';
+import Navigation from "./component/Navigation/Navigation";
+import Logo from './component/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-    <ImageLinkForm/>
-    <FaceRecognition/> */}
+      <Logo />
+      {/* <ImageLinkForm/>
+      <FaceRecognition/> */}
     </div>
   );
 }
